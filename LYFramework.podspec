fd204b53d28f7e39474abb292e207745cf70da90
林伟池 <loying@foxmail.com>
2016-05-25 14:46:17 +0800

    demo

--- LYFramework.podspec
+++ LYFramework.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name = 'LYFramework'
-s.version = '1.0.0'
+s.version = '1.0.1'
 s.license = 'MIT'
 s.summary = 'useful method'
 s.homepage = 'https://github.com/loyinglin/LYFramework'
---
+++ LYFrameworkDemo/LYFrameworkDemo/LYFrameworkDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  LYFrameworkDemo
+//
+//  Created by 林伟池 on 16/5/23.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LYFramework/Core/NSDictionary+LYDictToObject.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSDictionary* dict = @{@"abc":@"abc"};
+    [dict objectForClass:[NSNumber class]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
