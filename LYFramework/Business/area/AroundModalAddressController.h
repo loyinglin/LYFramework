aa9e7afcc3eb73da4f9d17c27d7e63641cefb647
林伟池 <loying@foxmail.com>
2016-08-05 09:06:14 +0800

    选择城市接口

---
+++ LYFramework/Business/area/AroundModalAddressController.h
@@ -0,0 +1,17 @@
+//
+//  AroundModalAddressController.h
+//  Yingying
+//
+//  Created by 林伟池 on 15/12/16.
+//  Copyright © 2015年 林伟池. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define NOTIFY_UI_SELECT_AREA       @"NOTIFY_UI_SELECT_AREA"    
+
+@interface AroundModalAddressController : UIViewController
+
+- (instancetype)initWithOverCurrentContext;
+
+@end
