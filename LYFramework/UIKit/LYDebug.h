2572cc0f99157a22a509de47b098a0884ae2fb96
林伟池 <loying@foxmail.com>
2016-08-25 09:56:07 +0800

    debug

--- LYFramework/UIKit/LYDebug.h
+++ LYFramework/UIKit/LYDebug.h
@@ -17,3 +17,12 @@
 #import <Foundation/Foundation.h>
 
 void LYDebug(const char *fileName, int lineNumber, NSString *fmt, ...);
+
+
+/**
+ *  在 Supporting Files 中的 info.plist 中打开 Application supports iTunes file sharing
+ *  把应用打包安装到手机中
+ *  将手机断开 iTunes 连接后再重新连接 iTunes（必要步骤），打开应用，找到应用，在右边的“应用名称”的文稿中找到以“ly”开头的 log 文件。
+ *  滚动到最下面，点击“存储到...”按钮将 Log 存储到桌面，双击打开就能看到 Log 信息了。
+ */
+void redirectNSlogToDocumentFolder();
--- LYFramework/UIKit/LYDebug.m
+++ LYFramework/UIKit/LYDebug.m
@@ -30,3 +30,24 @@ void LYDebug(const char *fileName, int lineNumber, NSString *fmt, ...)
     
     va_end(args);
 }
+
+
+void redirectNSlogToDocumentFolder() {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                         NSUserDomainMask, YES);
+    NSString *documentDirectory = [paths objectAtIndex:0];
+    
+    NSDate *currentDate = [NSDate date];
+    NSDateFormatter *dateformatter = [[NSDateFormatter alloc] init];
+    [dateformatter setDateFormat:@"MMddHHmmss"];
+    NSString *formattedDate = [dateformatter stringFromDate:currentDate];
+    
+    NSString *fileName = [NSString stringWithFormat:@"ly%@.log", formattedDate];
+    NSString *logFilePath =
+    [documentDirectory stringByAppendingPathComponent:fileName];
+    
+    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding], "a+",
+            stdout);
+    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding], "a+",
+            stderr);
+}
