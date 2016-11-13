229987207476374c7ca8c95d98adddc5d6473277
林伟池 <loying@foxmail.com>
2016-11-13 23:28:42 +0800

    f

---
+++ LYFramework/Business/FileUtil.h
@@ -0,0 +1,13 @@
+//
+//  FileUtil.h
+//  LYFrameworkDemo
+//
+//  Created by 林伟池 on 16/11/8.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileUtil : NSObject
+
+@end
---
+++ LYFramework/Business/FileUtil.m
@@ -0,0 +1,13 @@
+//
+//  FileUtil.m
+//  LYFrameworkDemo
+//
+//  Created by 林伟池 on 16/11/8.
+//  Copyright © 2016年 林伟池. All rights reserved.
+//
+
+#import "FileUtil.h"
+
+@implementation FileUtil
+
+@end
