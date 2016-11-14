d1ee00d96536e2e33c6cea382b1bb884235705b6
林伟池 <loying@foxmail.com>
2016-11-14 10:28:04 +0800

    文件

--- LYFramework/Business/FileUtil.h
+++ LYFramework/Business/FileUtil.h
@@ -10,4 +10,71 @@
 
 @interface FileUtil : NSObject
 
+
+#pragma mark - init
++ (instancetype)shareInstance;
+
+#pragma mark - update
+
+- (NSString *)docPath;		// 文档目录，需要ITUNES同步备份的数据存这里
+- (NSString *)libPrefPath;	// 配置目录，配置文件存这里
+- (NSString *)libCachePath;	// 缓存目录，系统永远不会删除这里的文件，ITUNES会删除
+- (NSString *)tmpPath;		// 缓存目录，APP退出后，系统可能会删除这里的内容
+- (NSString *) resPath:(NSString *)file;      // 资源目录
+
+
+
+#pragma mark - get
+
+/**
+ 目录，不存在就创建
+
+ @param path 目录路径
+ @return 返回是否成功
+ */
+- (BOOL)touch:(NSString *)path;
+
+
+/**
+ 文件
+
+ @param file 文件路径
+ @return 返回是否成功
+ */
+- (BOOL)touchFile:(NSString *)file;
+
+/**
+ * 创建目录
+ * api parameters 说明
+ * aPath 目录路径
+ */
+- (void)createDirectoryAtPath:(NSString *)aPath;
+
+/**
+ * 返回目下所有给定后缀的文件的方法
+ * api parameters 说明
+ *
+ * direString 目录绝对路径
+ * fileType 文件后缀名
+ * operation (预留,暂时没用)
+ */
+- (NSMutableArray *)allFilesAtPath:(NSString *)direString type:(NSString*)fileType operation:(int)operation;
+
+
+/**
+ * 返回目录文件的size,单位字节
+ * api parameters 说明
+ *
+ * filePath 目录路径
+ * diskMode 是否是磁盘占用的size
+ */
+- (uint64_t)sizeAtPath:(NSString *)filePath diskMode:(BOOL)diskMode;
+
+// 判断bundle里面是否存在该文件
+- (BOOL)judgeFileExistsInBundle:(NSString *)string;
+
+
+#pragma mark - message
+
+
 @end
--- LYFramework/Business/FileUtil.m
+++ LYFramework/Business/FileUtil.m
@@ -7,7 +7,234 @@
 //
 
 #import "FileUtil.h"
+#import <sys/stat.h>
+
+
+@interface FileUtil ()
+{
+    NSString *	_appPath;
+    NSString *	_docPath;
+    NSString *	_libPrefPath;
+    NSString *	_libCachePath;
+    NSString *	_tmpPath;
+}
+
+@end
 
 @implementation FileUtil
+
+#pragma mark - init
+
++ (instancetype)shareInstance {
+    static id test;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        test = [[[self class] alloc] init];
+    });
+    return test;
+}
+
+#pragma mark - update
+
+- (NSString *)appPath
+{
+    if ( nil == _appPath )
+    {
+        NSError * error = nil;
+        NSArray * paths = [[NSFileManager defaultManager] subpathsOfDirectoryAtPath:NSHomeDirectory() error:&error];
+        
+        for ( NSString * path in paths )
+        {
+            if ( [path hasSuffix:@".app"] )
+            {
+                _appPath = [NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), path];
+                break;
+            }
+        }
+    }
+    
+    return _appPath;
+}
+
+- (NSString *)docPath
+{
+    if ( nil == _docPath )
+    {
+        NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        _docPath = [paths objectAtIndex:0];
+    }
+    
+    return _docPath;
+}
+
+- (NSString *)libPrefPath
+{
+    if ( nil == _libPrefPath )
+    {
+        NSArray * paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+        NSString * path = [[paths objectAtIndex:0] stringByAppendingFormat:@"/Preference"];
+        
+        [self touch:path];
+        
+        _libPrefPath = path;
+    }
+    
+    return _libPrefPath;
+}
+
+- (NSString *)libCachePath
+{
+    if ( nil == _libCachePath )
+    {
+        NSArray * paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+        NSString * path = [[paths objectAtIndex:0] stringByAppendingFormat:@"/Caches"];
+        
+        [self touch:path];
+        
+        _libCachePath = path;
+    }
+    
+    return _libCachePath;
+}
+
+- (NSString *)tmpPath
+{
+    if ( nil == _tmpPath )
+    {
+        NSArray * paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+        NSString * path = [[paths objectAtIndex:0] stringByAppendingFormat:@"/tmp"];
+        
+        [self touch:path];
+        
+        _tmpPath = path;
+    }
+    
+    return _tmpPath;
+}
+
+-(NSString *) resPath:(NSString *)file{
+    NSString *str =[file stringByDeletingPathExtension];
+    NSString *str2 = [file pathExtension];
+    return [[NSBundle mainBundle] pathForResource:str ofType:str2];
+}
+
+- (BOOL)touch:(NSString *)path
+{
+    if ( NO == [[NSFileManager defaultManager] fileExistsAtPath:path] )
+    {
+        return [[NSFileManager defaultManager] createDirectoryAtPath:path
+                                         withIntermediateDirectories:YES
+                                                          attributes:nil
+                                                               error:NULL];
+    }
+    
+    return YES;
+}
+
+- (BOOL)touchFile:(NSString *)file
+{
+    if ( NO == [[NSFileManager defaultManager] fileExistsAtPath:file] )
+    {
+        return [[NSFileManager defaultManager] createFileAtPath:file
+                                                       contents:[NSData data]
+                                                     attributes:nil];
+    }
+    
+    return YES;
+}
+
+/***************************************************************/
+- (void)createDirectoryAtPath:(NSString *)aPath{
+    if ( NO == [[NSFileManager defaultManager] fileExistsAtPath:aPath isDirectory:NULL] )
+    {
+        BOOL ret = [[NSFileManager defaultManager] createDirectoryAtPath:aPath
+                                             withIntermediateDirectories:YES
+                                                              attributes:nil
+                                                                   error:nil];
+        if ( NO == ret ) {
+            return;
+        }
+    }
+}
+
+- (NSMutableArray *)allFilesAtPath:(NSString *)direString type:(NSString*)fileType operation:(int)operatio{
+    NSMutableArray *pathArray = [NSMutableArray array];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSArray *tempArray = [fileManager contentsOfDirectoryAtPath:direString error:nil];
+    
+    if (tempArray == nil) {
+        return nil;
+    }
+    
+    NSString* type = [NSString stringWithFormat:@".%@",fileType];
+    for (NSString *fileName in tempArray) {
+        BOOL flag = YES;
+        NSString *fullPath = [direString stringByAppendingPathComponent:fileName];
+        
+        if ([fileManager fileExistsAtPath:fullPath isDirectory:&flag]){
+            if (!flag) {
+                if ([fileName hasSuffix:type]) {
+                    [pathArray addObject:fullPath];
+                }
+            }
+            else {
+                
+            }
+        }
+    }
+    
+    return pathArray;
+}
+
+- (uint64_t)sizeAtPath:(NSString *)filePath diskMode:(BOOL)diskMode{
+    uint64_t totalSize = 0;
+    NSMutableArray *searchPaths = [NSMutableArray arrayWithObject:filePath];
+    while ([searchPaths count] > 0)
+    {
+        NSString *fullPath = [NSString stringWithString:[searchPaths objectAtIndex:0]];
+        [searchPaths removeObjectAtIndex:0];
+        
+        struct stat fileStat;
+        if (lstat([fullPath fileSystemRepresentation], &fileStat) == 0)
+        {
+            if (fileStat.st_mode & S_IFDIR)
+            {
+                NSArray *childSubPaths = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:fullPath error:nil];
+                for (NSString *childItem in childSubPaths)
+                {
+                    NSString *childPath = [fullPath stringByAppendingPathComponent:childItem];
+                    [searchPaths insertObject:childPath atIndex:0];
+                }
+            }
+            else
+            {
+                if (diskMode)
+                    totalSize += fileStat.st_blocks * 512;
+                else
+                    totalSize += fileStat.st_size;
+            }
+        }
+    }
+    
+    return totalSize;
+}
+
+// 判断bundle里面是否存在该文件
+- (BOOL)judgeFileExistsInBundle:(NSString *)string
+{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:string ofType:nil];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    return [fileManager fileExistsAtPath:filePath];
+}
+
+
+#pragma mark - get
+
+
+
+
+#pragma mark - message
 
 @end
