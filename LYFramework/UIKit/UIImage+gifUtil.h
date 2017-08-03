9a96fab16194e1ad356a3985325ae685e43e3f95
林伟池 <loying@foxmail.com>
2017-08-03 20:44:57 +0800

    gif播放的代码

---
+++ LYFramework/UIKit/UIImage+gifUtil.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+gifUtil.h
+//  LYFrameworkDemo
+//
+//  Created by 林伟池 on 2017/5/16.
+//  Copyright © 2017年 林伟池. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (gifUtil)
+
+-(void)decodeWithData:(NSData *)data;
+
+@end
